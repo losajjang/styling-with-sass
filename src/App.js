86010d576f8d48acb0e80ff1b65f407221d831a4
losajjang <losajjang@naver.com>
2022-08-28 21:11:28 +0900

    Sass 완료

--- src/App.js
+++ src/App.js
@@ -4,8 +4,52 @@ import Button from './components/Button';
 function App() {
   return (
     <div className="App">
-      <div className="Buttons">
+      <div className="buttons">
+        <Button size="large" onClick={() => console.log('clicked')}>
+          BUTTON
+        </Button>
         <Button>BUTTON</Button>
+        <Button size="small">BUTTON</Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" color="gray">
+          BUTTON
+        </Button>
+        <Button color="gray">BUTTON</Button>
+        <Button size="small" color="gray">
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" color="pink">
+          BUTTON
+        </Button>
+        <Button color="pink">BUTTON</Button>
+        <Button size="small" color="pink">
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" color="blue" outline>
+          BUTTON
+        </Button>
+        <Button color="gray" outline>
+          BUTTON
+        </Button>
+        <Button size="small" color="pink" outline>
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" fullWidth>
+          BUTTON
+        </Button>
+        <Button size="large" fullWidth color="gray">
+          BUTTON
+        </Button>
+        <Button size="large" fullWidth color="pink">
+          BUTTON
+        </Button>
       </div>
     </div>
   );
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,28 @@
 import React from 'react';
-import './Button.scss'
+import classNames from 'classnames';
+import './Button.scss';
 
-function Button({children}) {
-  return <button className="Button">{children}</button>;
+function Button({children, size, color, outline, fullWidth, ...rest}) {
+  // // join() 메소드 사용
+  // return <button className={['button', size].join(' ')}>{children}</button>;
+
+  // // 백틱 사용
+  // return <button className={`button ${size}`}>{children}</button>;
+
+  // classnames 라이브러리 사용
+  return (
+    <button
+      className={classNames('Button', size, color, {outline, fullWidth})}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
 }
+
+Button.defaultProps = {
+  size: 'medium',
+  color: 'blue',
+};
 
 export default Button;
